573de84e0d23874816c2324e51e8dc8a8a98538c
zhkrb <fiybad1@gmail.com>
2018-10-28 13:17:54 +0800

    rewrite code and update Readme

--- README.md
+++ README.md
@@ -8,13 +8,25 @@ Inspired by [cloudflare-scrape](https://github.com/Anorov/cloudflare-scrape).
 [eclipsesource/J2V8](https://github.com/eclipsesource/J2V8)  
 ### Example  
 ```java
-cloudflare cf = new cloudflare("eg.example.com");
-cf.set("Mozilla/5.0 XXXXXXXXXXX");
-List<HttpCookie> httpCookieList = cf.cookies();
+	Cloudflare cf = new Cloudflare(url);
+    cf.setUser_agent(UA);
+    cf.getCookies(new Cloudflare.cfCallback() {
+        @Override
+        public void onSuccess(List<HttpCookie> cookieList) {
+			something..
+        }
+
+        @Override
+        public void onFail() {
+            something..
+        }
+    });
 ```  
+ps: When you want to use cookieList for String, you needed call `Cloudflare.listToString(cookieList)` conversion to String.  
+  
 If you need to use jsoup  
 ```java
-Map<String, String> cookies = cf.List2Map(cf.cookies());
+Map<String, String> cookies = Cloudflare.List2Map(cookies);
 ```  
 ## issues
 If you find any issues, please file a bug after checking for duplicates so I can fix it.
--- cloudflare.java
+++ cloudflare.java
@@ -1,8 +1,10 @@
 package com.zhkrb.www.dmmagnet.aria2;
 
+import android.os.Looper;
+import android.text.TextUtils;
 import android.util.Log;
+
 import com.eclipsesource.v8.V8;
-import com.eclipsesource.v8.V8Object;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -23,147 +25,250 @@ import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-/**
- * Created by zhkrb on 2017/3/30.
- *
- * fix on 2018/10/17:change jschl_answer to double
- */
+public class Cloudflare {
 
-public class cloudflare {
+    private String mUrl;
+    private String mUser_agent;
+    private cfCallback mCallback;
+    private int mRetry_count;
+    private URL ConnUrl;
+    private List<HttpCookie> mCookieList;
+    private CookieManager mCookieManager;
+    private HttpURLConnection mCheckConn;
+    private HttpURLConnection mGetMainConn;
+    private HttpURLConnection mGetRedirectionConn;
 
-    private CookieManager cm;
-    private String UA;
-    private String url;
+    private static final int MAX_COUNT = 3;
+    private static final int CONN_TIMEOUT = 60000;
+    private static final String ACCEPT = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;";
 
+    private boolean canVisit = false;
 
-    public cloudflare(String url){
-        this.url = url;
+    public Cloudflare(String url) {
+        mUrl = url;
     }
 
-    public List<HttpCookie> cookies(){
-        cm = new CookieManager();
-        cm.setCookiePolicy(CookiePolicy.ACCEPT_ALL); //接受所有cookies
-        CookieHandler.setDefault(cm);
+    public Cloudflare(String url, String user_agent) {
+        mUrl = url;
+        mUser_agent = user_agent;
+    }
 
-        try {
-            /**
-             * 第一次链接得到响应头为503的DDOS page；
-             * 其他则直接返回
-             * 正则时没有匹配到也返回
-             */
-            URL ConnUrl = new URL(url);
-            HttpURLConnection conn= (HttpURLConnection) ConnUrl.openConnection();
-            conn.setRequestProperty("User-Agent",getUA());
-            conn.connect();
-            if (conn.getResponseCode() == 503){
-                InputStream is = conn.getErrorStream();
-                BufferedReader br = new BufferedReader(new InputStreamReader(is));
+    public String getUser_agent() {
+        return mUser_agent;
+    }
+
+    public void setUser_agent(String user_agent) {
+        mUser_agent = user_agent;
+    }
+
+    public void getCookies(final cfCallback callback){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                urlThread(callback);
+            }
+        }).start();
+    }
+
+    private void urlThread(cfCallback callback){
+        mCookieManager = new CookieManager();
+        mCookieManager.setCookiePolicy(CookiePolicy.ACCEPT_ALL); //接受所有cookies
+        CookieHandler.setDefault(mCookieManager);
+        HttpURLConnection.setFollowRedirects(false);
+
+        while (!canVisit){
+            if (mRetry_count>MAX_COUNT){
+                break;
+            }
+            try {
+
+                int responseCode = checkUrl();
+                if (responseCode==200){
+                    canVisit=true;
+                    break;
+                }else {
+                    getVisiteCookie();
+                }
+            } catch (IOException | InterruptedException e) {
+                if (mCookieList!=null){
+                    mCookieList.clear();
+                }
+                e.printStackTrace();
+            } finally {
+                closeAllConn();
+            }
+            mRetry_count++;
+        }
+        if (callback!=null){
+            Looper.prepare();
+            if (canVisit){
+                callback.onSuccess(mCookieList);
+            }else {
+                e("Get Cookie Failed");
+                callback.onFail();
+            }
+
+
+        }
+    }
+
+
+
+    private void getVisiteCookie() throws IOException, InterruptedException {
+        ConnUrl = new URL(mUrl);
+        mGetMainConn = (HttpURLConnection) ConnUrl.openConnection();
+        mGetMainConn.setRequestMethod("GET");
+        mGetMainConn.setConnectTimeout(CONN_TIMEOUT);
+        mGetMainConn.setReadTimeout(CONN_TIMEOUT);
+        if (!TextUtils.isEmpty(mUser_agent)){
+            mGetMainConn.setRequestProperty("user-agent",mUser_agent);
+        }
+        mGetMainConn.setRequestProperty("accept",ACCEPT);
+        mGetMainConn.setRequestProperty("referer", mUrl);
+        if (mCookieList!=null&&mCookieList.size()>0){
+            mGetMainConn.setRequestProperty("cookie",listToString(mCookieList));
+        }
+        mGetMainConn.setUseCaches(false);
+        mGetMainConn.connect();
+        switch (mGetMainConn.getResponseCode()){
+            case HttpURLConnection.HTTP_OK:
+                e("MainUrl","visit website success");
+                return;
+            case HttpURLConnection.HTTP_FORBIDDEN:
+                e("MainUrl","IP block or cookie err");
+                return;
+            case HttpURLConnection.HTTP_UNAVAILABLE:
+                InputStream mInputStream = mCheckConn.getErrorStream();
+                BufferedReader mBufferedReader = new BufferedReader(new InputStreamReader(mInputStream));
                 StringBuilder sb = new StringBuilder();
                 String str;
-                while ((str = br.readLine()) != null){
+                while ((str = mBufferedReader.readLine()) != null){
                     sb.append(str);
                 }
-                is.close();
-                br.close();
-
-                CookieStore ck = cm.getCookieStore();
-
-
-                conn.disconnect();
-
-
+                mInputStream.close();
+                mBufferedReader.close();
+                mCookieList = mCookieManager.getCookieStore().getCookies();
                 str = sb.toString();
-                System.out.println(str);
-                String jschl_vc = regex(str,"name=\"jschl_vc\" value=\"(.+?)\"").get(0);    //正则取值
-                String pass = regex(str,"name=\"pass\" value=\"(.+?)\"").get(0);            //
-                double jschl_answer = get_answer(str);                                         //计算结果
-                System.out.println(jschl_answer);
+                getCheckAnswer(str);
+                break;
+            default:
 
-                Thread.sleep(4000);
-
-                /*
-                  第二次链接得到相应头为302的页面并取得cookies；
-                 */
-                String req = String.valueOf("https://"+ConnUrl.getHost())+"/cdn-cgi/l/chk_jschl?"
-                        +"jschl_vc="+jschl_vc+"&pass="+pass+"&jschl_answer="+jschl_answer;
-                System.out.println(req);
-                HttpURLConnection.setFollowRedirects(false);
-                HttpURLConnection reqconn = (HttpURLConnection) new URL(req).openConnection();
-
-                reqconn.setRequestProperty("Referer", req);
-                reqconn.setRequestProperty("User-Agent",getUA());
-                reqconn.setRequestProperty("Cookie",ck.getCookies().toString());
-                reqconn.connect();
-                if (reqconn.getResponseCode() ==302){
-                    CookieStore ck1 = cm.getCookieStore();
-                    reqconn.disconnect();
-                    System.out.println(reqconn.getHeaderFields());
-
-                    /*
-                      同上
-                     */
-                    HttpURLConnection conn302 = (HttpURLConnection) new URL(req).openConnection();
-                    conn302.setRequestProperty("Referer",ConnUrl.getHost());
-                    conn302.setRequestProperty("User-Agent",getUA());
-                    conn302.setRequestProperty("Cookie",ck1.getCookies().toString());
-                    conn302.connect();
-                    if (conn302.getResponseCode() == 302){
-                        System.out.println("conn302:302");
-                        Log.i("conn302",conn302.getHeaderFields().toString());
-                        CookieStore ck2 = cm.getCookieStore();
-                        Log.i("conn302",ck2.getCookies().toString());
-                        conn302.disconnect();
-                        return ck2.getCookies();
-                    }
-                }
-            }else {
-                CookieStore cookieStore = cm.getCookieStore();
-                return cookieStore.getCookies();
-
-            }
-        }catch (NullPointerException e){
-            Log.e("Err","Must set UA");
-        }catch (IndexOutOfBoundsException e){
-            CookieStore cookieStore = cm.getCookieStore();
-            return cookieStore.getCookies();
+                break;
         }
-        catch (IOException | InterruptedException e){
-            e.printStackTrace();
+    }
+
+    /**
+     * 获取值并跳转获得cookies
+     * @param str
+     */
+    private void getCheckAnswer(String str) throws InterruptedException, IOException {
+        String jschl_vc = regex(str,"name=\"jschl_vc\" value=\"(.+?)\"").get(0);    //正则取值
+        String pass = regex(str,"name=\"pass\" value=\"(.+?)\"").get(0);            //
+        double jschl_answer = get_answer(str);
+        e(String.valueOf(jschl_answer));
+        Thread.sleep(3000);
+        String req = String.valueOf("https://"+ConnUrl.getHost())+"/cdn-cgi/l/chk_jschl?"
+                +"jschl_vc="+jschl_vc+"&pass="+pass+"&jschl_answer="+jschl_answer;
+        e("RedirectUrl",req);
+        getRedirectResponse(req);
+    }
+
+    private void getRedirectResponse(String req) throws IOException {
+        HttpURLConnection.setFollowRedirects(false);
+        mGetRedirectionConn = (HttpURLConnection) new URL(req).openConnection();
+        mGetRedirectionConn.setRequestMethod("GET");
+        mGetRedirectionConn.setConnectTimeout(CONN_TIMEOUT);
+        mGetRedirectionConn.setReadTimeout(CONN_TIMEOUT);
+        if (!TextUtils.isEmpty(mUser_agent)){
+            mGetRedirectionConn.setRequestProperty("user-agent",mUser_agent);
         }
-        return null;
+        mGetRedirectionConn.setRequestProperty("accept",ACCEPT);
+        mGetRedirectionConn.setRequestProperty("referer", req);
+        if (mCookieList!=null&&mCookieList.size()>0){
+            mGetRedirectionConn.setRequestProperty("cookie",listToString(mCookieList));
         }
+        mGetRedirectionConn.setUseCaches(false);
+        mGetRedirectionConn.connect();
+        switch (mGetRedirectionConn.getResponseCode()){
+            case HttpURLConnection.HTTP_OK:
+                mCookieList = mCookieManager.getCookieStore().getCookies();
+                break;
+            case HttpURLConnection.HTTP_MOVED_TEMP:
+                mCookieList = mCookieManager.getCookieStore().getCookies();
+                break;
+            default:throw new IOException("getOtherResponse Code: "+
+                    mGetRedirectionConn.getResponseCode());
+        }
+    }
+
+
+    private int checkUrl()throws IOException {
+        URL ConnUrl = new URL(mUrl);
+        mCheckConn = (HttpURLConnection) ConnUrl.openConnection();
+        mCheckConn.setRequestMethod("GET");
+        mCheckConn.setConnectTimeout(CONN_TIMEOUT);
+        mCheckConn.setReadTimeout(CONN_TIMEOUT);
+        if (!TextUtils.isEmpty(mUser_agent)){
+            mCheckConn.setRequestProperty("user-agent",mUser_agent);
+        }
+        mCheckConn.setRequestProperty("accept",ACCEPT);
+        mCheckConn.setRequestProperty("referer",mUrl);
+        if (mCookieList!=null&&mCookieList.size()>0){
+            mCheckConn.setRequestProperty("cookie",listToString(mCookieList));
+        }
+        mCheckConn.setUseCaches(false);
+        mCheckConn.connect();
+        return mCheckConn.getResponseCode();
+    }
+
+    private void closeAllConn(){
+        if (mCheckConn!=null){
+            mCheckConn.disconnect();
+        }
+        if (mGetMainConn!=null){
+            mGetMainConn.disconnect();
+        }
+        if (mGetRedirectionConn!=null){
+            mGetRedirectionConn.disconnect();
+        }
+    }
+
+
+    public interface cfCallback{
+        void onSuccess(List<HttpCookie> cookieList);
+        void onFail();
+    }
 
     private double get_answer(String str) {  //取值
         double a = 0;
 
         try {
-        List<String> s = regex(str,"var s,t,o,p,b,r,e,a,k,i,n,g,f, (.+?)=\\{\"(.+?)\"");
-        System.out.println(s);
-        String varA = s.get(0);
-        String varB = s.get(1);
-        StringBuilder sb = new StringBuilder();
-        sb.append("var a=");
-        sb.append(regex(str,varA+"=\\{\""+varB+"\":(.+?)\\}").get(0));
-        sb.append(";");
-        List<String> b = regex(str,varA+"\\."+varB+"(.+?)\\;");
-        for (int i =0;i<b.size()-1;i++){
-            sb.append("a");
-            sb.append(b.get(i));
+            List<String> s = regex(str,"var s,t,o,p,b,r,e,a,k,i,n,g,f, " +
+                    "(.+?)=\\{\"(.+?)\"");
+            String varA = s.get(0);
+            String varB = s.get(1);
+            StringBuilder sb = new StringBuilder();
+            sb.append("var a=");
+            sb.append(regex(str,varA+"=\\{\""+varB+"\":(.+?)\\}").get(0));
             sb.append(";");
-        }
+            List<String> b = regex(str,varA+"\\."+varB+"(.+?)\\;");
+            for (int i =0;i<b.size()-1;i++){
+                sb.append("a");
+                sb.append(b.get(i));
+                sb.append(";");
+            }
 
-        Log.i("add",sb.toString());
-        V8 v8 = V8.createV8Runtime();
-        a = v8.executeDoubleScript(sb.toString());
-        List<String> fixNum = regex(str,"toFixed\\((.+?)\\)");
-        if (fixNum!=null){
-            a = Double.parseDouble(v8.executeStringScript(String.valueOf(a)+".toFixed("+fixNum.get(0)+");"));
-        }
-
-        a += new URL(url).getHost().length();
-        v8.release();
+            e("add",sb.toString());
+            V8 v8 = V8.createV8Runtime();
+            a = v8.executeDoubleScript(sb.toString());
+            List<String> fixNum = regex(str,"toFixed\\((.+?)\\)");
+            if (fixNum!=null){
+                a = Double.parseDouble(v8.executeStringScript("String("+String.valueOf(a)+".toFixed("+fixNum.get(0)+"));"));
+            }
+            a += new URL(mUrl).getHost().length();
+            v8.release();
         }catch (IndexOutOfBoundsException e){
-            Log.e("answerErr","get answer error");
+            e("answerErr","get answer error");
             e.printStackTrace();
         }
         catch (MalformedURLException e) {
@@ -172,16 +277,13 @@ public class cloudflare {
         return a;
     }
 
-    private String getUA() {
-        return UA;
-        }
-
-    public void setUA(String UA) {
-        this.UA = UA;
-        }
-
-
-    private List<String> regex(String text, String pattern){    //正则
+    /**
+     * 正则
+     * @param text 本体
+     * @param pattern 正则式
+     * @return List<String>
+     */
+    private List<String> regex(String text, String pattern){
         try {
             Pattern pt = Pattern.compile(pattern);
             Matcher mt = pt.matcher(text);
@@ -193,8 +295,6 @@ public class cloudflare {
                         group.add(mt.group(1));
                         group.add(mt.group(2));
                     }else group.add(mt.group(1));
-
-
                 }
             }
             return group;
@@ -204,22 +304,52 @@ public class cloudflare {
         return null;
     }
 
-    public Map<String,String> List2Map(List<HttpCookie> list){  //转换为jsoup可用的map
-         Map<String,String> map= new HashMap<>();
-         try {if (list!=null){
-             for (int i=0;i<list.size();i++){
-                 String[] listStr = list.get(i).toString().split("=");
-                 map.put(listStr[0],listStr[1]);
-             }
-             Log.i("List2Map",map.toString());
-         }else return map;
+    /**
+     * 转换list为 ; 符号链接的字符串
+     * @param list
+     * @return
+     */
+    public static String listToString(List list ) {
+        char separator = ";".charAt(0);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < list.size(); i++) {
+            sb.append(list.get(i)).append(separator);
+        }
+        return sb.toString().substring(0, sb.toString().length() - 1);
+    }
 
-         }catch (IndexOutOfBoundsException e){
-             e.printStackTrace();
-         }
 
+    /**
+     * 转换为jsoup可用的Hashmap
+     * @param list  HttpCookie列表
+     * @return Hashmap
+     */
+    public static Map<String,String> List2Map(List<HttpCookie> list){
+        Map<String, String> map = new HashMap<>();
+        try {
+            if (list != null) {
+                for (int i = 0; i < list.size(); i++) {
+                    String[] listStr = list.get(i).toString().split("=");
+                    map.put(listStr[0], listStr[1]);
+                }
+                Log.i("List2Map", map.toString());
+            } else {
+                return map;
+            }
+
+        } catch (IndexOutOfBoundsException e) {
+            e.printStackTrace();
+        }
 
         return map;
+    }
+
+    private void e(String tag,String content){
+        Log.e(tag,content);
+    }
+
+    private void e(String content){
+        Log.e("cloudflare",content);
     }
 
 }
