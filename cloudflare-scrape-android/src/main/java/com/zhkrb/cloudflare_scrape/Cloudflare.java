91af9581dafae39c50541038a8cd2d0cb2710003
zhkrb <fiybad1@gmail.com>
2020-02-01 21:08:34 +0800

    add http code check

--- cloudflare-scrape-android/src/main/java/com/zhkrb/cloudflare_scrape/Cloudflare.java
+++ cloudflare-scrape-android/src/main/java/com/zhkrb/cloudflare_scrape/Cloudflare.java
@@ -144,6 +144,7 @@ public class Cloudflare {
                 checkCookie(mCookieList);
                 return;
             case HttpURLConnection.HTTP_MOVED_PERM:
+            case HttpURLConnection.HTTP_MOVED_TEMP:
                 mUrl = mGetMainConn.getHeaderField("Location");
                 mCookieList = mCookieManager.getCookieStore().getCookies();
                 checkCookie(mCookieList);
